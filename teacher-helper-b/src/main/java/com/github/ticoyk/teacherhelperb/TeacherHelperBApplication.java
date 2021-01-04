d74b85eeb3dbf1f38b19c8c6c5961e9c45dad80b
TicoYk <didimoykym@gmail.com>
2021-01-04 08:58:42 -0300

    first commit

---
+++ teacher-helper-b/src/main/java/com/github/ticoyk/teacherhelperb/TeacherHelperBApplication.java
@@ -0,0 +1,13 @@
+package com.github.ticoyk.teacherhelperb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherHelperBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherHelperBApplication.class, args);
+	}
+
+}
