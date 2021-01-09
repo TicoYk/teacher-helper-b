6c0262c0453b275799fea400159307aa8785536e
TicoYk <didimoykym@gmail.com>
2021-01-08 21:39:14 -0300

    Created Entity/Pojo for Student

---
+++ teacher-helper-b/src/main/java/com/github/ticoyk/teacherhelperb/models/Student.java
@@ -0,0 +1,39 @@
+package com.github.ticoyk.teacherhelperb.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    
+    private String name;
+
+    public Student(){ }
+
+    public Student(String name){
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
