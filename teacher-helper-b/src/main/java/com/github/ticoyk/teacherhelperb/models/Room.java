97129130cbab115f9c2a8dcf584974a3dded6000
TicoYk <didimoykym@gmail.com>
2021-01-10 22:03:47 -0300

    Created Room model

---
+++ teacher-helper-b/src/main/java/com/github/ticoyk/teacherhelperb/models/Room.java
@@ -0,0 +1,81 @@
+package com.github.ticoyk.teacherhelperb.models;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Room {
+
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    private String id;
+
+    private String name;
+    
+    private Long columns;
+    private Long rows;
+
+    @ManyToOne
+    private Set<Desk> desks;
+
+    public Room() { }
+
+    public Room(String name, Long columns, Long rows, Set<Desk> desks) {
+        this.name = name;
+        this.columns = columns;
+        this.rows = rows;
+        this.desks = desks;
+    }
+
+    public Room(String name, Long columns, Long rows) {
+        this.name = name;
+        this.columns = columns;
+        this.rows = rows;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Long columns) {
+        this.columns = columns;
+    }
+
+    public Long getRows() {
+        return rows;
+    }
+
+    public void setRows(Long rows) {
+        this.rows = rows;
+    }
+
+    public Set<Desk> getDesks() {
+        return desks;
+    }
+
+    public void setDesks(Set<Desk> desks) {
+        this.desks = desks;
+    }
+    
+}
