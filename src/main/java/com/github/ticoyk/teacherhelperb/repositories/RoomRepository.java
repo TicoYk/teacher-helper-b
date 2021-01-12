5ff685bef9d2d782727e2a70ddbab9ae60b209d0
TicoYk <didimoykym@gmail.com>
2021-01-12 17:15:45 -0300

    Created Repository to work with postgres, #1

---
+++ src/main/java/com/github/ticoyk/teacherhelperb/repositories/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.github.ticoyk.teacherhelperb.repositories;
+
+import com.github.ticoyk.teacherhelperb.models.Room;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long>{
+    
+}
--- src/main/java/com/github/ticoyk/teacherhelperb/repositories/StudentRepository.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/repositories/StudentRepository.java
@@ -3,9 +3,7 @@ package com.github.ticoyk.teacherhelperb.repositories;
 import com.github.ticoyk.teacherhelperb.models.Student;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StudentRepository extends CrudRepository<Student, Long>{
     
 }
