5a0b5256820b6c1ef502125be945c5970763f6a1
Dídimo Yokoyama <didimoykym@gmail.com>
2021-01-24 23:39:15 -0300

    Created Initial Encoder for User password, #3

---
+++ src/main/java/com/github/ticoyk/teacherhelperb/services/UserService.java
@@ -0,0 +1,16 @@
+package com.github.ticoyk.teacherhelperb.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
