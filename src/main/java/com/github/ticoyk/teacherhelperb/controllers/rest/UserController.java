22e55fe7128918e4b3495b9fe1f0233e688e332c
Dídimo <didimoykym@gmail.com>
2021-02-24 11:22:15 -0300

    Created User Controller

---
+++ src/main/java/com/github/ticoyk/teacherhelperb/controllers/rest/UserController.java
@@ -0,0 +1,8 @@
+package com.github.ticoyk.teacherhelperb.controllers.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+}
