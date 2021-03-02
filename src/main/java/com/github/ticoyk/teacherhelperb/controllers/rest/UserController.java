7425b70eb02cf0157d6531353526943cdb5f0848
TicoYk <didimoykym@gmail.com>
2021-03-02 16:05:37 -0300

    Removed unnecessary Imports

--- src/main/java/com/github/ticoyk/teacherhelperb/controllers/rest/UserController.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/controllers/rest/UserController.java
@@ -1,13 +1,9 @@
 package com.github.ticoyk.teacherhelperb.controllers.rest;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.github.ticoyk.teacherhelperb.configurations.security.SecurityConstants;
 import com.github.ticoyk.teacherhelperb.models.ApplicationUser;
 import com.github.ticoyk.teacherhelperb.utils.JwtUtil;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
