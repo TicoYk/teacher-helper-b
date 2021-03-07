5d1296b86419d3a15249994c46b8b11171d53881
TicoYk <didimoykym@gmail.com>
2021-03-07 18:33:55 -0300

    Included method to find User related data

--- src/main/java/com/github/ticoyk/teacherhelperb/repositories/RoomRepository.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/repositories/RoomRepository.java
@@ -5,5 +5,7 @@ import com.github.ticoyk.teacherhelperb.models.Room;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoomRepository extends CrudRepository<Room, Long>{
-    
+    Room findByIdAndApplicationUserId(Long id, Long applicationUserId);
+    Iterable<Room> findByApplicationUserId(Long applicationUserId);
+    void deleteByIdAndApplicationUserId(Long id, Long applciationUserId);
 }
--- src/main/java/com/github/ticoyk/teacherhelperb/repositories/StudentRepository.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/repositories/StudentRepository.java
@@ -5,5 +5,7 @@ import com.github.ticoyk.teacherhelperb.models.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long>{
-    
+    Student findByIdAndApplicationUserId(Long id, Long applicationUserId);
+    Iterable<Student> findByApplicationUserId(Long applicationUserId);
+    void deleteByIdAndApplicationUserId(Long id, Long applciationUserId);
 }
