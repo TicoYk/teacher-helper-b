514579d31f4db0a0505e3f545fd75d8157a4a9fc
TicoYk <didimoykym@gmail.com>
2021-03-08 10:57:22 -0300

    Updated to init data only when dev profile is active

--- src/main/java/com/github/ticoyk/teacherhelperb/configurations/SetupData.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/configurations/SetupData.java
@@ -6,6 +6,7 @@ import com.github.ticoyk.teacherhelperb.configurations.security.BCryptPassword;
 import com.github.ticoyk.teacherhelperb.models.ApplicationUser;
 import com.github.ticoyk.teacherhelperb.repositories.ApplicationUserRepository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,10 @@ public class SetupData {
 
     private ApplicationUserRepository userRepository;
     private BCryptPassword bCryptPassword;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
     public SetupData(ApplicationUserRepository userRepository, BCryptPassword bCryptPassword){
         this.bCryptPassword = bCryptPassword;
         this.userRepository = userRepository;
@@ -20,21 +25,23 @@ public class SetupData {
     
     @PostConstruct
     public void init() {
-        initUsers();
+        if(activeProfile.equals("dev")){
+            initUsers();
+        }
     }
 
     private void initUsers() {
         
         ApplicationUser user1 = new ApplicationUser();
-        user1.setName("john");
-        user1.setEmail("somemail1@somehost.com");
-        user1.setPassword(bCryptPassword.encode("123"));
+        user1.setName("joao");
+        user1.setEmail("joao@somemail.com");
+        user1.setPassword(bCryptPassword.encode("password"));
         userRepository.save(user1);
         
         ApplicationUser user2 = new ApplicationUser();
-        user2.setName("tom");
-        user2.setEmail("somemail2@somehost.com");
-        user2.setPassword("111");
+        user2.setName("pedro");
+        user2.setEmail("pedro@somemail.com");
+        user2.setPassword(bCryptPassword.encode("password"));
         userRepository.save(user2);
     }
 
