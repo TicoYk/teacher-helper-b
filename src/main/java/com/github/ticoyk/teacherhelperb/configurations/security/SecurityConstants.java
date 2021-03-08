a3bcf4c6ad27c99a2557aecce1938cd46caae892
TicoYk <didimoykym@gmail.com>
2021-03-08 04:07:46 -0300

    Updated to use env vars

--- src/main/java/com/github/ticoyk/teacherhelperb/configurations/security/SecurityConstants.java
+++ src/main/java/com/github/ticoyk/teacherhelperb/configurations/security/SecurityConstants.java
@@ -1,10 +1,25 @@
 package com.github.ticoyk.teacherhelperb.configurations.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SecurityConstants {
-    public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static String SECRET;
+    public static Long EXPIRATION_TIME;
+
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String REGISTER_URL = "/api/users";
     public static final String LOGIN_URL = "/api/login";
+
+    @Autowired
+    public void loadSecurityConfig(
+            @Value("${jwt.security.config.secret}") String secret,
+            @Value("${jwt.security.config.expiration-time}") String expirationTime
+        ){
+        SECRET = secret;
+        EXPIRATION_TIME = Long.parseLong(expirationTime);
+    }
 }
